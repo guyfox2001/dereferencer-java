6b9c6dfe906c70b3c3b46513d12cb49d91372cf7
Boris Stupin <boris.stupin.01@gmail.com>
2022-01-11 17:07:47 +0300

    add tests and move Application.java to tests

---
+++ src/test/java/ru/fusionsoft/iu/dereferencer/factories/ReferenceFactoryTest.java
@@ -0,0 +1,23 @@
+package ru.fusionsoft.iu.dereferencer.factories;
+
+import org.junit.jupiter.api.Test;
+import ru.fusionsoft.iu.dereferencer.exceptions.InvalidReferenceException;
+import ru.fusionsoft.iu.dereferencer.reference.LocalReference;
+import ru.fusionsoft.iu.dereferencer.reference.Reference;
+
+import java.net.URI;
+
+class ReferenceFactoryTest {
+
+
+    @Test
+    void  makeGitHubReference(){
+
+    }
+
+    @Test
+    void makeLocalReference () throws InvalidReferenceException {
+        Reference assertValue = new LocalReference(URI.create("W:/info-universe"));
+        Reference expectingValue = ReferenceFactory.makeReference("W:/info-universe");
+    }
+}
---
+++ src/test/java/ru/fusionsoft/iu/dereferencer/managers/impl/GitManagerTest.java
@@ -0,0 +1,11 @@
+package ru.fusionsoft.iu.dereferencer.managers.impl;
+
+class GitManagerTest {
+
+//    @Test
+//    void findFile() throws URISyntaxException {
+//        File assertation = new File("iuData/Sample-univ/Nomenclature.yaml");
+//        File Equals = GitManager.findFile(new URI("https://github.com/guyfox2001/dataHolder.git::main::newdir/Nomenclature.yaml"));
+//        assertEquals(assertation, Equals);
+//    }
+}
